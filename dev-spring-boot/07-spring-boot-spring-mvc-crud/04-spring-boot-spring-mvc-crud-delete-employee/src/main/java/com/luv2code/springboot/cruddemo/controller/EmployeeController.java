7fd80b955b101e5437118c27010d363c617adac5
aferin3 <aferfidan3@gmail.com>
2024-08-12 15:08:32 +0300

    mvc crud

---
+++ dev-spring-boot/07-spring-boot-spring-mvc-crud/04-spring-boot-spring-mvc-crud-delete-employee/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,72 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        // get the employees from db
+        List<Employee> employees = employeeService.findAll();
+        // add to the spring model
+        model.addAttribute("employees", employees);
+
+        return "employees/list-employees";
+
+    }
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        //create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int employeeId, Model model) {
+
+        //get the employee from the service
+        Employee employee = employeeService.findById(employeeId);
+
+        // set employee in the model to prepopulate the form
+        model.addAttribute("employee", employee);
+
+        //send over to our form
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int employeeId) {
+
+        // delete the employee
+        employeeService.deleteById(employeeId);
+
+        // redirect to /employees/list
+        return "redirect:/employees/list";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        //save the employee
+        employeeService.save(employee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+}
---
+++ dev-spring-boot/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/fidan/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,4 @@
+package com.fidan.springboot.demosecurity.controller;
+
+public class DemoController {
+}
